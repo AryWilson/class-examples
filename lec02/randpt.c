987fbaaa4453f063ebd6f35837951dc415906b7f
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-02-01 17:56:33 +0000

    clas excersize

--- lec02/randpt.c
+++ lec02/randpt.c
@@ -1,7 +1,17 @@
+#include <math.h>
+#include <stdlib.h>
 #include <stdio.h>
 
+void rand_point(int* x, int* y, int* z){
+	*x = rand()%10;
+	*y = rand()%10;
+	*z = rand()%10;
+
+}
+
 void main() {
-  int x,y,z;
-  rand_point(&x, &y, &z);
-  printf("point: %d, %d, %d\n", x, y, z);
+	srand(time(0)); //set the seed of the program to currest system time, so random give dif values each time
+  	int x,y,z;
+  	rand_point(&x, &y, &z);
+  	printf("point: %d, %d, %d\n", x, y, z);
 }
