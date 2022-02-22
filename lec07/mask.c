3d5a92417b94cc1563c0895e3896bb80f86389a9
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-02-22 12:59:36 -0500

    update before class

---
+++ lec07/mask.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  int value = 0;
+  printf("enter a value :");
+  scanf(" %i",&value);
+
+  unsigned int mask = 10000;
+  int result = mask & value;
+  printf("\n value = %i, mask = %i, out= %i\n"val);
+
+}
--- lec07/overflow3.c
+++ lec07/overflow3.c
@@ -2,7 +2,7 @@
 
 int main() {
   unsigned int a = 0;
-  for (a = 5; a >= 0; a--) {
+  for (a = 1; a >= 0; a--) {
     printf("Message!\n");
   }
 
