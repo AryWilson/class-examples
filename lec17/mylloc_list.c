2be9e42fe74b254c6c2a148eaf6b91a457831bde
Ary Wilson <awilson@goldengate.cs.brynmawr.edu>
2022-03-29 13:10:07 -0400

    before class

--- lec17/mylloc_list.c
+++ lec17/mylloc_list.c
@@ -12,15 +12,24 @@ void *malloc (size_t size) {
   if (size == 0){
     return NULL;
   }
-  void *memory = sbrk(size);
+
+
+//check free list for memory, if found remove from free list and return address
+
+  void *memory = sbrk(size + sizeof(struct chunk));
   if (memory == (void *) -1) {
     return NULL;
   } else {
-    return memory;
+    struct chunk* chunk = (struct chunk) memory;
+    chunk->size = size;
+    chunk->next = NULL;
+    return (void*) (chunk + 1);
+    //return the memory location + sizeof(chunk)
   }
 }
 
 void free(void *memory) {
+  // add memory to free list
   return;
 }
 
